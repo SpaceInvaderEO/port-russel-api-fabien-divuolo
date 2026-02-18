326b2f3f645dc402f415f7e82f811c84a4ab0138
FabienDiVuolo <divuolo.fabien@gmail.com>
2026-02-18 15:38:55 +0100

    ok pour mongodb et mongoose

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connecté'))
+    .catch(err => console.error('Erreur MongoDB :', err));
  
 app.get('/', (req, res) => {
-    res.send('Serveur Port Russell OK');
+    res.send('Serveur Port Russell OKKKKKK avec MongoDB');
 });
  
 app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Serveur démarré avec succès sur http://localhost:${PORT}`);
 });
